f4b40e6ca4be3abc50c7436d7c49ad05bac0ae51
DavidHladky <hladkyd@gmail.com>
2016-07-09 08:32:21 +0200

    Add files via upload

--- HD44780.h
+++ HD44780.h
@@ -39,9 +39,9 @@
 
 void display_HD44780_init(); // Initialization of HD44780
 
-void display_HD44780_cmd(uint8_t cmd_to_disp); // Send command to the HD44780
+void display_HD44780_cmd(uint8_t cmd_to_disp); // Send the command to the HD44780
 
-void display_HD44780_data(uint8_t data); // Write data to the memory of HD44780
+void display_HD44780_data(uint8_t data); // Write the data to the memory of HD44780
 
 int8_t display_HD44780_write_char(char c, uint8_t row, uint8_t position); // Write a character to a specific position; maximum number of rows is 2; range of number for position is from 1 to 16.
 
